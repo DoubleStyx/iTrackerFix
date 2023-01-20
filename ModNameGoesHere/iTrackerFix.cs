380b9d1aeff440faba55132bd43886d3706e18d0
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2023-01-20 04:12:26 -0600

    updates

---
+++ ModNameGoesHere/iTrackerFix.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using FrooxEngine.CommonAvatar;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Linq;
+using System.Reflection;
+
+namespace SU2GaugeGroup
+{
+    public class iTrackerFix : NeosMod
+    {
+        public override string Name => "iTrackerFix";
+        public override string Author => "DoubleStyx";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/DoubleStyx/iTrackerFix";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.doublestyx.iTrackerFix");
+            harmony.PatchAll();
+        }
+
+        
+        [HarmonyPatch]
+        public static class EyeManager_OnCommonUpdate
+        {
+
+            [HarmonyPatch(typeof(EyeManager), "OnCommonUpdate")]
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> OnAttach_Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = instructions.ToList();
+                int index = codes.FindLastIndex(x => x.opcode == OpCodes.Call && ((MethodInfo)x.operand).Name == "get_HasValue");
+
+                codes[index+1].opcode = OpCodes.Br;
+
+                codes.Insert(index+1, new CodeInstruction(OpCodes.Pop));
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
+//         ****     **                          *******                             **                        
+//        /**/**   /**                         /**////**                           /**                        
+//        /**//**  /**  *****   ******   ******/**   /**   *****   ******  ******  /** **    **  *****  ******
+//        /** //** /** **///** **////** **//// /*******   **///** **////  **////** /**/**   /** **///**//**//*
+//        /**  //**/**/*******/**   /**//***** /**///**  /*******//***** /**   /** /**//** /** /******* /** / 
+//        /**   //****/**//// /**   /** /////**/**  //** /**////  /////**/**   /** /** //****  /**////  /**   
+//        /**    //***//******//******  ****** /**   //**//****** ****** //******  ***  //**   //******/***   
+//        //      ///  //////  //////  //////  //     //  ////// //////   //////  ///    //     ////// ///    
+// Hey guys check out this c00l html 6.7 surround sound website called NEOSResolver ඞ
